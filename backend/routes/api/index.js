118cf3b0877f3b0ee6afb6a2b09ee5cf3eb16b97
Mark Osman <markrockwellosman@gmail.com>
2022-04-28 13:03:46 -0500

    feat: Add user login/logout routes, tested functionality with fetch

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,50 @@
 const router = require("express").Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+/************** TESTING IMPORTS *************/
+// const asyncHandler = require('express-async-handler');
+// const { User } = require('../../db/models');
+// const { restoreUser, requireAuth, setTokenCookie } = require('../../utils/auth.js');
+
+/************** TESTING ROUTES **************/
+/*
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'DemoUser'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+// GET /api/restore-user
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// GET /api/require-auth
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+*/
+
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
+//POST /api/test
 router.post("/test", function (req, res) {
     res.json({ requestBody: req.body });
 });
