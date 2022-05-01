4bcc4a0425298cc003e7a6d298593dc95a6ef8d0
Mark <mark@marks-mbp.lan>
2022-05-01 18:43:12 -0500

    feat: Add sidebar navigation collapse and styling

--- frontend/src/components/BrowserNavigation/index.js
+++ frontend/src/components/BrowserNavigation/index.js
@@ -1,36 +1,96 @@
-import React from 'react';
-import { NavLink, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './BrowserNavigation.css';
+import React from "react";
+import { NavLink, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./BrowserNavigation.css";
 
-function BrowserNavigation({ isLoaded }){
-//   const sessionUser = useSelector(state => state.session.user);
+function CreateNoteButton() {
+    return <div>+New</div>;
+}
 
-//   let sessionLinks;
-//   if (sessionUser) {
-//     sessionLinks = (
-//       <ProfileButton user={sessionUser} />
-//     );
-//   } else {
-//     Redirect
-//   }
+function BrowserNavigation({ sidebarOpen, isLoaded }) {
+    //   const sessionUser = useSelector(state => state.session.user);
 
-  return (
-    <div>
-        <ProfileButton />
-    <ul>
+    //   let sessionLinks;
+    //   if (sessionUser) {
+    //     sessionLinks = (
+    //       <ProfileButton user={sessionUser} />
+    //     );
+    //   } else {
+    //     Redirect
+    //   }
 
-      <li>
+    return (
+        <div className="sidebar" style={sidebarOpen ? { width: '208px'} : {width: '60px'}}>
+            <ProfileButton sidebarOpen={sidebarOpen}/>
+            <CreateNoteButton />
+            <ul className="sidebar-ul">
+                <li className="sidebar-li">
+                    <NavLink
+                        className="sidebar-navlink"
+                        exact
+                        to="/browser/dashboard"
+                    >
+                        <img
+                            className="sidebar-link-img"
+                            id="home-btn"
+                            alt="home"
+                            src="/images/home-deselected.svg"
+                        />
+                        <span style={sidebarOpen ? { display: 'flex'} : {display: 'none'}}>Home</span>
+                    </NavLink>
+                </li>
+                <li className="sidebar-li">
+                    <NavLink
+                        className="sidebar-navlink"
+                        exact
+                        to="/browser/notes"
+                    >
+                        <img
+                            className="sidebar-link-img"
+                            id="notes-btn"
 
-        <NavLink exact to="/browser/dashboard">Home</NavLink>
-        <NavLink exact to="/browser/notes">Notes</NavLink>
-        <NavLink exact to="/browser/notebooks">Notebooks</NavLink>
-        <NavLink exact to="/browser/tags">Tags</NavLink>
-      </li>
-    </ul>
-    </div>
-  );
+                            alt="notes"
+                            src="/images/notes-deselected.svg"
+                        />
+                        <span style={sidebarOpen ? { display: 'flex'} : {display: 'none'}}>Notes</span>
+                    </NavLink>
+                </li>
+                <li className="sidebar-li">
+                    <NavLink
+                        className="sidebar-navlink"
+                        exact
+                        to="/browser/notebooks"
+                    >
+                        <img
+                            className="sidebar-link-img"
+                            id="notebooks-btn"
+
+                            alt="notebooks"
+                            src="/images/notebooks.svg"
+                        />
+                        <span style={sidebarOpen ? { display: 'flex'} : {display: 'none'}}>Notebooks</span>
+                    </NavLink>
+                </li>
+                <li className="sidebar-li">
+                    <NavLink
+                        className="sidebar-navlink"
+                        exact
+                        to="/browser/tags"
+                    >
+                        <img
+                            className="sidebar-link-img"
+                            id="tags-btn"
+
+                            alt="tags"
+                            src="/images/tags.svg"
+                        />
+                        <span style={sidebarOpen ? { display: 'flex'} : {display: 'none'}}>Tags</span>
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default BrowserNavigation;
